bd4ed73ee44b658d89603abbcf5e9e1fe6372320
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-23 17:02:01 +0200

    Repository
    repositories

---
+++ src/main/java/com/enviro/enviro/assessment/grad001/chumanimadikizela/repository/WasteCategoryRepository.java
@@ -0,0 +1,9 @@
+package com.enviro.enviro.assessment.grad001.chumanimadikizela.repository;
+
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.entity.WasteCategoryEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class WasteCategoryRepository extends JpaRepository<WasteCategoryEntity, Long> {
+}
