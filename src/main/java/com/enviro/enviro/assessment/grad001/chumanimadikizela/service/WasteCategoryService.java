ba94666e2471707aaea16c579bb3856aeab81e0a
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-24 15:33:59 +0200

    waste category service
    adding interface of waste category service and implementing its service

--- src/main/java/com/enviro/enviro/assessment/grad001/chumanimadikizela/service/WasteCategoryService.java
+++ src/main/java/com/enviro/enviro/assessment/grad001/chumanimadikizela/service/WasteCategoryService.java
@@ -1,4 +1,14 @@
 package com.enviro.enviro.assessment.grad001.chumanimadikizela.service;
 
-public class WasteCategoryService {
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.entity.WasteCategoryEntity;
+
+import java.util.List;
+
+public interface WasteCategoryService {
+
+    WasteCategoryEntity save(WasteCategoryEntity wasteCategoryEntity);
+    WasteCategoryEntity update(WasteCategoryEntity wasteCategoryEntity);
+    List<WasteCategoryEntity> findAll();
+    WasteCategoryEntity findById(Long id);
+    void deleteById(Long id);
 }
--- src/main/java/com/enviro/enviro/assessment/grad001/chumanimadikizela/service/impl/WasteCategoryServiceImpl.java
+++ src/main/java/com/enviro/enviro/assessment/grad001/chumanimadikizela/service/impl/WasteCategoryServiceImpl.java
@@ -1,4 +1,45 @@
 package com.enviro.enviro.assessment.grad001.chumanimadikizela.service.impl;
 
-public class WasteCategoryServiceImpl {
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.entity.WasteCategoryEntity;
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.exceptions.NotFoundException;
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.repository.WasteCategoryRepository;
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.service.WasteCategoryService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class WasteCategoryServiceImpl implements WasteCategoryService {
+
+    private final WasteCategoryRepository wasteCategoryRepository;
+
+    @Override
+    public WasteCategoryEntity save(WasteCategoryEntity wasteCategoryEntity) {
+        return wasteCategoryRepository.save(wasteCategoryEntity);
+    }
+
+    @Override
+    public WasteCategoryEntity update(WasteCategoryEntity wasteCategoryEntity) {
+        WasteCategoryEntity dbWasteCategory = findById(wasteCategoryEntity.getId());
+        dbWasteCategory.setCategory(wasteCategoryEntity.getCategory());
+        return save(dbWasteCategory);
+    }
+
+    @Override
+    public List<WasteCategoryEntity> findAll() {
+        return wasteCategoryRepository.findAll();
+    }
+
+    @Override
+    public WasteCategoryEntity findById(Long id) {
+        return wasteCategoryRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Could not find the waste category"));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        wasteCategoryRepository.deleteById(id);
+    }
 }
