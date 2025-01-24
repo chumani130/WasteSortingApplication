e6c7be43886cde1fc5c296e1a0d0dc2ba48374cd
Chumani Madikizela <56044652+chumani130@users.noreply.github.com>
2025-01-24 15:33:30 +0200

    recycling tips service
    adding interface of recycling tips service and implementing its service

--- src/main/java/com/enviro/enviro/assessment/grad001/chumanimadikizela/service/RecyclingTipsService.java
+++ src/main/java/com/enviro/enviro/assessment/grad001/chumanimadikizela/service/RecyclingTipsService.java
@@ -1,4 +1,13 @@
 package com.enviro.enviro.assessment.grad001.chumanimadikizela.service;
 
-public class RecyclingTipsService {
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.entity.RecyclingTipsEntity;
+
+import java.util.List;
+
+public interface RecyclingTipsService {
+
+    RecyclingTipsEntity save(RecyclingTipsEntity recyclingTipsEntity);
+    List<RecyclingTipsEntity> findAll();
+    RecyclingTipsEntity findById(Long id);
+    void deleteById(Long id);
 }
--- src/main/java/com/enviro/enviro/assessment/grad001/chumanimadikizela/service/impl/RecyclingTipsServiceImpl.java
+++ src/main/java/com/enviro/enviro/assessment/grad001/chumanimadikizela/service/impl/RecyclingTipsServiceImpl.java
@@ -1,4 +1,38 @@
 package com.enviro.enviro.assessment.grad001.chumanimadikizela.service.impl;
 
-public class RecyclingTipsServiceImpl {
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.entity.RecyclingTipsEntity;
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.exceptions.NotFoundException;
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.repository.RecyclingTipsRepository;
+import com.enviro.enviro.assessment.grad001.chumanimadikizela.service.RecyclingTipsService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class RecyclingTipsServiceImpl implements RecyclingTipsService {
+
+    private final RecyclingTipsRepository recyclingTipsRepository;
+
+    @Override
+    public RecyclingTipsEntity save(RecyclingTipsEntity recyclingTipsEntity) {
+        return recyclingTipsRepository.save(recyclingTipsEntity);
+    }
+
+    @Override
+    public List<RecyclingTipsEntity> findAll() {
+        return recyclingTipsRepository.findAll();
+    }
+
+    @Override
+    public RecyclingTipsEntity findById(Long id) {
+        return recyclingTipsRepository.findById(id)
+                .orElseThrow(() -> new NotFoundException("Could not find the recycling tips with id: " + id));
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        recyclingTipsRepository.deleteById(id);
+    }
 }
